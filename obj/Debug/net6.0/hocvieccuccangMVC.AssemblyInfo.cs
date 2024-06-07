d850e515f54593f46388b1aaba9cdf2988718e0c
Hwuan2007 <thquan200703@gmail.com>
2024-06-07 09:54:48 +0700

    fix done

--- obj/Debug/net6.0/hocvieccuccangMVC.AssemblyInfo.cs
+++ obj/Debug/net6.0/hocvieccuccangMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hocvieccuccangMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1122500be22ef7485125b8243cc0972ea86966")]
 [assembly: System.Reflection.AssemblyProductAttribute("hocvieccuccangMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hocvieccuccangMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
